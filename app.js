17be5af2cfd1ddb4ff0470a9fd758f864d9ec770
Joseph Lokahi Podwys <jpodwys@ldschurch.org>
2015-09-13 18:06:15 -0600

    Adding more demos.

--- app.js
+++ app.js
@@ -6,8 +6,18 @@ var app = express();
 var stream = require('express-stream');
 app.set('views', './views');
 app.set('view engine', 'ejs');
-
 var PORT = process.env.PORT || 3000;
+
+
+
+
+
+function cache(req, res, next) {
+  res.set({
+    'Cache-Control': 'public, max-age=5000'
+  });
+  next();
+}
 
 
 
@@ -23,6 +33,16 @@ app.get('/data', function (req, res) {
 
 
 
+app.get('/name', function (req, res) {
+  setTimeout(function(){
+    res.send({name: 'Joe Podwys'});
+  }, 1000);
+});
+
+
+
+
+
 app.get('/', function (req, res) {
   res.render('index');
 });
@@ -31,7 +51,7 @@ app.get('/', function (req, res) {
 
 
 
-app.get('/no-stream', function (req, res) {
+app.get('/no-stream', cache, function (req, res) {
   superagent
     .get('http://localhost:' + PORT + '/data')
     .end(function (err, response){
@@ -44,7 +64,7 @@ app.get('/no-stream', function (req, res) {
 
 
 
-app.get('/manual-stream', function (req, res){
+app.get('/manual-stream', cache, function (req, res){
   var headerFile = fs.readFileSync(__dirname + '/views/stream-header.ejs', {encoding: 'utf-8'});
   res.write(headerFile);
   var template = ejs.compile(fs.readFileSync(__dirname + '/views/stream-body.ejs', 'utf8'));
@@ -60,6 +80,8 @@ app.get('/manual-stream', function (req, res){
 
 
 
+
+
 //One-time, app-wide express-stream config
 stream.openHtmlOpenHead(true);
 stream.closeHeadOpenBody('stream-close-head-open-body');
@@ -67,11 +89,43 @@ stream.closeBodyCloseHtml(true);
 stream.streamBefore('stream-head');
 
 //Route using express-stream
-app.get('/express-stream', stream.stream(), function (req, res){
+app.get('/express-stream', cache, stream.stream(), function (req, res){
   superagent
     .get('http://localhost:' + PORT + '/data')
     .end(function (err, response){
       res.render('stream-body');
+    }
+  );
+});
+
+
+
+
+
+stream.wrapJavascript(true);
+
+app.get('/express-stream-pipe', cache, stream.pipe(), function (req, res){
+  res.stream('pipe');
+  superagent
+    .get('http://localhost:' + PORT + '/name')
+    .end(function (err, response){
+      res.pipe("$('.font21.serif').html('Joe Podwys');");
+      res.close();
+    }
+  );
+});
+
+
+
+
+
+app.get('/express-stream-pipe-view', cache, stream.pipe(), function (req, res){
+  res.stream('pipe');
+  superagent
+    .get('http://localhost:' + PORT + '/name')
+    .end(function (err, response){
+      res.stream('pipe-view', {name: response.body.name});
+      res.close();
     }
   );
 });
